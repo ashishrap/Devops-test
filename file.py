30e195499b7b69d3fcb1de103fc89dde06a92605
ashishrap <77382885+ashishrap@users.noreply.github.com>
2023-02-26 13:27:08 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+f=open("C:\\Users\\RapriaAS\\Desktop\\files\\python\\text.txt",'w')
+
+with open("C:\\Users\\RapriaAS\\Desktop\\files\\python\\text.txt",'w')as f:
+    f.write("this is my first file\n")
+    f.write("not the second file\n")
+    
+f.close()
